5a8cd68dfeab02347ef284d80dde2e5292059776
1NT9NS9 <fedor.ivanov.agri.1985@gmail.com>
2025-12-10 15:22:57 +0600

    I've decided to make my commercial project publicly available. The project compiles a news digest based on your Telegram channel!

--- bot/services/__init__.py
+++
@@ -1,7 +0,0 @@
-# Services package initialization
-from bot.services.storage import StorageService
-from bot.services.ai import AIService
-from bot.services.scraper import ScraperService
-from bot.services.clustering import ClusteringService
-
-__all__ = ['StorageService', 'AIService', 'ScraperService', 'ClusteringService']
